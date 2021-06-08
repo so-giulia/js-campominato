9d01a9c8d907d647d6b03247f11ec07d30892dd7
GiuliaSoccolini <giuliasoccolini@yahoo.it>
2021-06-08 16:57:00 +0200

    ottimizzazione codice

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-//**OK**Il computer deve generare 16 numeri casuali tra 1 e 100.
-//**OK**I numeri non possono essere duplicati.
+//Il computer deve generare 16 numeri casuali tra 1 e 100.
+//I numeri non possono essere duplicati.
 
-//**OK**In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-//**OK**L’utente non può inserire più volte lo stesso numero.
+//In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+//L’utente non può inserire più volte lo stesso numero.
 
-//**OK**Se il numero è presente nella lista dei numeri generati, la partita termina,
-//**OK**altrimenti si continua chiedendo all’utente un altro numero.
+//Se il numero è presente nella lista dei numeri generati, la partita termina,
+//altrimenti si continua chiedendo all’utente un altro numero.
 
 //La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 
-//**OK**Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito
 
 
 
@@ -17,55 +17,67 @@
 //SVOLGIMENTO
 //———————————
 
-//creo una funzione che mi genera i numeri random
+var intMin = 1;
+var intMax = 100;
+var subtract = 16;
+
+//!F:genera i numeri random
 function generateRandom(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//!F:ritorna la presenza di un elemento in un array
+function isInArray(array, el){
+    return array.includes(el);
 }
 
 //genero 16 numeri univoci per il computer
 var programArray = [];
 
 while(programArray.length < 16){
+    var random = generateRandom(intMin,intMax);
 
-    var random = generateRandom(1,100);
-
-    if(!programArray.includes(random)){
+    if(!isInArray(programArray, random)){
         programArray.push(random);
     }
 }
 
-//chiedo all'utente (per il momento 100-97 volte) un numero
-var userNum = [];
+var userArray = [];
 
-for(var i = 0; i < 84; i++){
-    
+var check = false;
+var messaggio = "";
+
+//chiedo all'utente un numero
+for(var i = 0; i < (intMax - subtract) && !check; i++){
+    //chiedo numero
     var requestNum = parseInt(prompt("Inserisci un numero compreso tra 1 e 100"));
-
+    
     //non posso inserire un numero non compreso tra 1 e 100
-    if(requestNum < 1 || requestNum > 100){
-        document.getElementById("risultati").innerHTML = "Il numero deve essere compreso tra 1 e 100, rigioca la partita!"
-        break;
+    if(requestNum < intMin || requestNum > intMax){
+        messaggio = "Il numero deve essere compreso tra 1 e 100, rigioca la partita!"
+        check = true;
     }
 
     //l'utente non può inserire il numero due volte sennò finisce tutto
-    if(!userNum.includes(requestNum)){
-        userNum.push(requestNum);
-    }else{
-        document.getElementById("risultati").innerHTML = "Non puoi inserire un numero due volte, rigioca la partita!"
-        break;
+    if(!isInArray(userArray, requestNum) && !check){
+        userArray.push(requestNum);
+    }else if(isInArray(userArray, requestNum) && !check){
+        check = true;
+        messaggio = "Non puoi inserire un numero due volte, rigioca la partita!"
     }
 
     //condizione di perdita partita
-    if(programArray.includes(requestNum)){
+    if(isInArray(programArray, requestNum) && !check){
+        check = true;
         alert("Hai perso!");
         //comunico quante volte ho inserito i numeri giusti
-        document.getElementById("risultati").innerHTML += "Hai totalizzato: " + (userNum.length-1) + " punti";
-        break;
+        messaggio = "Hai totalizzato: " + (userArray.length-1) + " punti";
     }
-
 }
+
+document.getElementById("risultati").innerHTML = messaggio;
 
 //controlli console
 console.log(programArray);
-console.log(userNum);
+console.log(userArray);
 
