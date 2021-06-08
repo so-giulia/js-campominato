c56d28e242b79bee3100c1d0e5eb80fd787a0dcc
GiuliaSoccolini <giuliasoccolini@yahoo.it>
2021-06-08 17:29:10 +0200

    add: bonus

--- js/script.js
+++ js/script.js
@@ -11,7 +11,12 @@
 
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito
 
-
+//BONUS:
+//all’inizio il software richiede anche una difficoltà all’utente
+//che cambia il range di numeri casuali:
+//difficoltà 0 => tra 1 e 100
+//difficoltà 1 => tra 1 e 80
+//difficoltà 2 => tra 1 e 50
 
 //———————————
 //SVOLGIMENTO
@@ -19,7 +24,29 @@
 
 var intMin = 1;
 var intMax = 100;
+var userMax = 100;
 var subtract = 16;
+
+//Bonus
+var chooseLevel = false;
+
+while(!chooseLevel){
+    chooseLevel = prompt("Scegli la difficoltà tra 0, 1 e 2");
+
+    if(chooseLevel == "0" || chooseLevel == "difficoltà 0"){
+        intMax = 100;
+        console.log("Difficoltà 0, numero massimo: " + intMax);
+    }else if(chooseLevel == "1" || chooseLevel == "difficoltà 1"){
+        intMax = 80;
+        console.log("Difficoltà 1, numero massimo: " + intMax);
+    }else if(chooseLevel == "2" || chooseLevel == "difficoltà 2"){
+        intMax = 50;
+        console.log("Difficoltà 2, numero massimo: " + intMax);
+    }else{
+        chooseLevel = false;
+    }
+}
+
 
 //!F:genera i numeri random
 function generateRandom(min, max){
@@ -48,13 +75,16 @@ var check = false;
 var messaggio = "";
 
 //chiedo all'utente un numero
-for(var i = 0; i < (intMax - subtract) && !check; i++){
+for(var i = 0; i < (userMax - subtract) && !check; i++){
     //chiedo numero
     var requestNum = parseInt(prompt("Inserisci un numero compreso tra 1 e 100"));
     
     //non posso inserire un numero non compreso tra 1 e 100
-    if(requestNum < intMin || requestNum > intMax){
+    if(requestNum < intMin || requestNum > userMax){
         messaggio = "Il numero deve essere compreso tra 1 e 100, rigioca la partita!"
+        check = true;
+    }else if(isNaN(requestNum) || requestNum == "null" || requestNum == "undefined"){
+        messaggio = "Inserisci un numero compreso tra 1 e 100, rigioca la partita!"
         check = true;
     }
 
@@ -80,4 +110,3 @@ document.getElementById("risultati").innerHTML = messaggio;
 //controlli console
 console.log(programArray);
 console.log(userArray);
-
