bc8c0f4757c10b6d7021352a731c4b496abb298e
GiuliaSoccolini <giuliasoccolini@yahoo.it>
2021-06-08 14:49:43 +0200

    add: parte computer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-//Il computer deve generare 16 numeri casuali tra 1 e 100.
-//I numeri non possono essere duplicati.
+//**OK**Il computer deve generare 16 numeri casuali tra 1 e 100.
+//**OK**I numeri non possono essere duplicati.
 //In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 //L’utente non può inserire più volte lo stesso numero.
 //Se il numero è presente nella lista dei numeri generati, la partita termina,
@@ -9,3 +9,29 @@
 
 //Al termine della partita il software deve comunicare il punteggio,
 //cioè il numero di volte che l’utente ha inserito un numero consentito
+
+
+
+//———————————
+//SVOLGIMENTO
+//———————————
+
+//creo una funzione che mi genera i numeri random
+function generateRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//genero 16 numeri univoci per il computer
+var programArray = [];
+
+while(programArray.length < 16){
+
+    var random = generateRandom(1,100);
+
+    if(!programArray.includes(random)){
+        programArray.push(random);
+    }
+}
+
+//li guardo
+console.log(programArray);
